9e05ef813039ec45649147ebfcce1bdb09eb7105
jumli_d <148221429+jumli404@users.noreply.github.com>
2024-01-12 01:46:13 +0530

    Create project.sql

---
+++ project.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Student (
+    Roll_no int (2),
+    Name varchar (20),
+    Class int (2),
+    Subject varchar (15),
+    Absent int (100)
+);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (1, 'poonam', 11, 'IT', 20);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (2, 'surya', 11, 'IT', 30);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (3, 'Ranjit', 11, 'IT', 30);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (4, 'Roshni', 11, 'IT', 20);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (5, 'Renuka', 11, 'IT', 20);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (6, 'Suresh', 11, 'IT', 40);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (7, 'Divya', 11, 'IT', 50);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (8, 'Roshni', 11, 'IT', 30);
+INSERT INTO Student (Roll_no, Name, Class, Subject, Absent)
+VALUES (9, 'Krishna', 11, 'IT', 20);
